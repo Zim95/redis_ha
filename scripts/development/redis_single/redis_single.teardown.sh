f6b2e29d6447f40b496f0c6e9701ef1c11685c67
Zim95 <shresthanamah@gmail.com>
2025-10-04 11:36:00 +0530

    Modified teardown script for redis single

--- scripts/development/redis_single/redis_single.teardown.sh
+++ scripts/development/redis_single/redis_single.teardown.sh
@@ -8,15 +8,6 @@ fi
 NAMESPACE=$1
 export NAMESPACE=$NAMESPACE
 
-echo "Tearing down Redis single instance..."
+REDIS_SINGLE_YAML=./redis_single/redis-single.yaml
 
-# Delete resources
-kubectl delete pod browseterm-redis -n "$NAMESPACE" --ignore-not-found=true
-kubectl delete service browseterm-redis-service -n "$NAMESPACE" --ignore-not-found=true
-kubectl delete pvc browseterm-redis-pvc -n "$NAMESPACE" --ignore-not-found=true
-kubectl delete pv browseterm-redis-pv -n "$NAMESPACE" --ignore-not-found=true
-kubectl delete secret redis-password -n "$NAMESPACE" --ignore-not-found=true
-
-echo "Redis single instance teardown complete!"
-echo ""
-echo "Note: Redis data in /data/browseterm-redis is preserved for manual cleanup if needed."
+envsubst < $REDIS_SINGLE_YAML | kubectl delete -n "$NAMESPACE" -f -
